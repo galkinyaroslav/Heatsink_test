1cd6b8c02905814cfcbcd13c01dc545fbe6c251e
Yaroslav Galkin <galkinyaroslav@gmail.com>
2023-08-09 17:07:54 +0300

    changed functions in meas_control.py

--- src/measurements/meas_control.py
+++ src/measurements/meas_control.py
@@ -1,59 +1,50 @@
-import os
-
 import pyvisa as visa
-import time
 
 
-def open_device():
+def find_device():
     rmag = visa.ResourceManager()  # '/usr/lib/x86_64-linux-gnu/libiovisa.so'
     list_devices = rmag.list_resources('?*')
+    print(list_devices)
     DEVISE_NAME = '34970A'
     for device in list_devices:
-        temp_device = rmag.open_resource(device)
         try:
-            if DEVISE_NAME in temp_device.query('*IDN?'):
-                print(temp_device.query('*IDN?').strip(), ' --->>> OPENED')
+            temp_device = rmag.open_resource(device)
+            temp_name = temp_device.query('*IDN?')
+            if DEVISE_NAME in temp_name:
+                print(temp_name.strip(), '--->>>', device)
+                # temp_device.close()
                 return temp_device
 
         except visa.errors.VisaIOError:
-            print('Devise is not found')
+            print('This is not appropriate device or device is not found')
 
 
 def configure(device):
-    # a34970.write_termination = '\n'
-    # a34970.read_termination = '\n'
-    # print(device.query('*IDN?'))
-    device.write('*cls')
-    device.write('*rst')
-    #
+    a34970.write_termination = '\n'
+    a34970.read_termination = '\n'
+    print(f"{device.query('*IDN?')} --->>> CONFIGURED")
+    device.write('*RST')
+    device.write('*CLS')
+
     device.write('CONF:TEMP FRTD, 85 ,(@202, 203)')
     device.write('TEMP:TRAN:FRTD:RES:REF 1000,(@202, 203)')
 
-    # print(device.query('TEMP:TRAN:FRTD:RES:REF? (@202, 203)'))
+    print(device.query('TEMP:TRAN:FRTD:RES:REF? (@202, 203)'))
 
     device.write('TEMP:TRAN:FRTD:TYPE 85,(@202, 203)')
-    # print(device.query('TEMP:TRAN:FRTD:TYPE? (@202, 203)'))
+    print(device.query('TEMP:TRAN:FRTD:TYPE? (@202, 203)'))
 
-    # print(device.query('CONF? (@202, 203)'))
+    print(device.query('CONF? (@202, 203)'))
     device.write('ROUT:SCAN (@202, 203)')
 
 
 def read_data(device):
-    data = [float(value) for value in device.query('READ?').strip().split(',')]
+    row_data = device.query('READ?')
+    data = [float(value) for value in row_data.strip().split(',')]
     print(data)
-    # time.sleep(1)
 
 
-a34970 = open_device()
-configure(a34970)
-read_data(a34970)
-# data = a34970.query('READ?')
-# print(type(data))
-
-
-
-
-# TODO write_async works correctly, but after that query goes to error
-# print(f'session {a34970.session}')
-# a34970.visalib.write_asynchronously(a34970.session, 'TEMP:TRAN:FRTD:RES:REF 100,(@202)'.encode())
-# a34970.visalib.read_asynchronously(a34970.session, 'TEMP:TRAN:FRTD:RES:REF 1000,(@202)'.encode())
+if __name__ == '__main__':
+    a34970 = find_device()
+    configure(a34970)
+    read_data(a34970)
